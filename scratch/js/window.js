7df7f06c96e036202eb777697151e56594b5f62c
Garrett Smith <19smitgr@users.noreply.github.com>
2016-08-19 09:26:08 -0400

    Prevent arrow key scrolling

--- scratch/js/window.js
+++ scratch/js/window.js
@@ -10,6 +10,12 @@ function setSize() // make glossy front page responsive
 }
 
 gloss.addEventListener("resize", setSize());
+window.addEventListener("keydown", function(e) {
+    // space and arrow keys
+    if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+}, false);
 
 $("#play").click(function(){ // slides glossy front page up
     document.getElementById("gloss").removeEventListener("resize", setSize())
