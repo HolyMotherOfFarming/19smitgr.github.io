336e25ca8fde7d291ac32e0de91fa5840b79e9ee
Garrett Smith <19smitgr@users.noreply.github.com>
2016-08-26 17:16:23 -0400

    Move ball painting game from root directory

---
+++ Ball Painting/scratch/js/window.js
@@ -0,0 +1,23 @@
+// This file is for the initial setup of the window; not the game
+
+var gloss = document.getElementById("gloss");
+
+function setSize() // make glossy front page responsive
+{
+    gloss.style.width = window.innerWidth + "px";
+    gloss.style.height = window.innerHeight + "px";
+    gloss.style.top = gloss.style.height * .25 + "px";
+}
+
+gloss.addEventListener("resize", setSize, false);
+window.addEventListener("keydown", function(e) {
+    // space and arrow keys
+    if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+}, false);
+
+$(".glossSlide").click(function(){ // slides glossy front page up
+    $("#gloss").slideToggle("slow");
+})
+
